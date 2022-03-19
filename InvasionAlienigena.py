4e14aff4e3e72c0e1772dac3ad4efb9b8b0725b2
Jonathan Cerrito <jcerritocampos@gmail.com>
2022-03-19 17:41:32 -0600

    Configuraciones basicas Invasion Alienigena

---
+++ InvasionAlienigena.py
@@ -0,0 +1,26 @@
+from asyncio import run
+import sys
+import pygame
+
+from Settings import Settings
+
+def run_game():                                                                             # Definimos el metodo que inicializa el juego, las configuraciones y el objeto screen
+    pygame.init()                                                                           # Inicializa la configuracion de pygame
+    settings = Settings()                                                                   # Settings nos permite importar las configuraciones del juego
+    
+    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))       # Creamos la pantalla de visualizacion indicando en pixeles el ancho y el alto
+    pygame.display.set_caption("Invasión Alienígena")                                       # Asignamos el titulo de la ventana
+
+    bg_color = (settings.bg_color)                                                          # Creamos una variable para establecer el color de fondo en la pantalla
+
+    while True:                                                                             # Bucle de activacion de juego
+        
+        for event in pygame.event.get():                                                    # Ciclo de eventos para controlar el juego
+            if event.type == pygame.QUIT:                                                   # Si el usuario da click en el boton de cierre de la ventana
+                sys.exit()                                                                  # Se cierra todo proceso y termina el juego
+
+        screen.fill(bg_color)                                                               # Dibujamos la pantalla en cada ciclo asignando el color de fondo
+        
+        pygame.display.flip()                                                               # Indica a pygame que dibuje una nueva pantalla y actualiza los espacios
+
+run_game()                                                                                  # Ejecutamos el metodo que inicializa el juego
---
+++ Settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """ Esta clase almacenará todas las configuraciones """
+
+    def __init__(self):                                   # Inicializamos las configuraciones del juego
+        self.screen_width = 800                           # Asignamos un valor para el ancho de la ventana del juego
+        self.screen_height = 600                          # Asignamos un valor pata el alto de la ventana del juego
+        self.bg_color = (230, 230, 230)                   # Asignamos en formato rgb un color para el fondo de la ventana
+        
