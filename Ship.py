c7b8536edae5c89d0c8739da9eb8e7751d08e377
Jonathan Cerrito <jcerritocampos@gmail.com>
2022-03-20 17:07:22 -0600

    Limitar area de movimiento de la nave

--- Ship.py
+++ Ship.py
@@ -3,31 +3,31 @@ import pygame
 class Ship():
     """ Clase para controlar el comportamienot de la nave """
 
-    def __init__(self, settings, screen):                     # Metodo encargado de inicializar la nave y establecer la posicion de inicio
+    def __init__(self, settings, screen):                                   # Metodo encargado de inicializar la nave y establecer la posicion de inicio
         self.screen = screen
-        self.settings = settings                              # Con la variable settings podemos hacer uso de las configuraciones
+        self.settings = settings                                            # Con la variable settings podemos hacer uso de las configuraciones
 
-        self.image = pygame.image.load("images/ship.bmp")     # Cargamos la imagen de la nave
-        self.rect = self.image.get_rect()                     # Obtenemos el rect de la superficie para mostrar la nave
-        self.screen_rect = screen.get_rect()                  # Obtenemos el rect de la pantalla
+        self.image = pygame.image.load("images/ship.bmp")                   # Cargamos la imagen de la nave
+        self.rect = self.image.get_rect()                                   # Obtenemos el rect de la superficie para mostrar la nave
+        self.screen_rect = screen.get_rect()                                # Obtenemos el rect de la pantalla
 
-        self.rect.centerx = self.screen_rect.centerx          # La coordenada rect x del centro de la nave la igualamos a la coordenada rect x del centro de la pantalla
-        self.rect.bottom = self.screen_rect.bottom            # La coordenada rect y de la parte inferior de la nave la igualamos a la coordenada rect y de la parte inferior de la pantalla
+        self.rect.centerx = self.screen_rect.centerx                        # La coordenada rect x del centro de la nave la igualamos a la coordenada rect x del centro de la pantalla
+        self.rect.bottom = self.screen_rect.bottom                          # La coordenada rect y de la parte inferior de la nave la igualamos a la coordenada rect y de la parte inferior de la pantalla
 
-        self.center = float(self.rect.centerx)                # Almacenamos un valor decimal para el centro de la nave
+        self.center = float(self.rect.centerx)                              # Almacenamos un valor decimal para el centro de la nave
 
-        self.movingRight = False                              # Bandera de movimiento de la nave hacia la derecha
-        self.movingLeft = False                               # Bandera de movimiento de la nave hacia la izquierda
+        self.movingRight = False                                            # Bandera de movimiento de la nave hacia la derecha
+        self.movingLeft = False                                             # Bandera de movimiento de la nave hacia la izquierda
 
-    def update(self):                                         # Actualiza la posicion de la nave acorde  al bandera de movimiento
-        if self.movingRight:                                  # Verificamos si la bandera de movimiento a la derecha se encuentra activa
-            self.center += self.settings.shipSpeedFactor      # Si la bandera de movimiento a la derecha se encuentra activa movemos la nave a la derecha
+    def update(self):                                                       # Actualiza la posicion de la nave acorde  al bandera de movimiento
+        if self.movingRight and self.rect.right < self.screen_rect.right:   # Verificamos si la bandera de movimiento a la derecha se encuentra activa y que su posicion no salga de la pantalla
+            self.center += self.settings.shipSpeedFactor                    # Si la bandera de movimiento a la derecha se encuentra activa movemos la nave a la derecha
         
-        if self.movingLeft:                                   # Verificamos si la bandera de movimiento a la izquierda se encuentra activa
-            self.center -= self.settings.shipSpeedFactor      # Si la bandera de movimiento a la izquierda se encuentra activa movemos la nave a la izquierda
+        if self.movingLeft and self.rect.left > 0:                          # Verificamos si la bandera de movimiento a la izquierda se encuentra activa y que su posicion no salga de la pantalla
+            self.center -= self.settings.shipSpeedFactor                    # Si la bandera de movimiento a la izquierda se encuentra activa movemos la nave a la izquierda
 
-        self.rect.centerx = self.center                       # Aplicamos el movimiento para que este se muestre en pantalla
+        self.rect.centerx = self.center                                     # Aplicamos el movimiento para que este se muestre en pantalla
 
-    def blitme(self):                                         # Metodo para posicionar la nave en su posicion actual
-        self.screen.blit(self.image, self.rect)               # Posiciona la imagen en la pantalla para visualizarla
+    def blitme(self):                                                       # Metodo para posicionar la nave en su posicion actual
+        self.screen.blit(self.image, self.rect)                             # Posiciona la imagen en la pantalla para visualizarla
         
