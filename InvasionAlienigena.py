494a849d7e0af395a99db211004a0c07e3545af4
jcerrito <jcerritocampos@gmail.com>
2022-03-27 12:58:17 -0600

    Almacenamiento de balas en un grupo

--- InvasionAlienigena.py
+++ InvasionAlienigena.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 import GameFunctions as gameFunctions                                                       # Importamos GameFunctions y sus funciones las guardamos en una variable
 
 from Settings import Settings                                                               # Del archivo Settings importamos la clase Settings
@@ -12,11 +13,13 @@ def run_game():                                                                 
     pygame.display.set_caption("Invasión Alienígena")                                       # Asignamos el titulo de la ventana
 
     ship = Ship(settings, screen)                                                           # Creamos una nave en pantalla
+    bullets = Group()                                                                       # Grupo para almacenas lar las balas
     bg_color = (settings.bg_color)                                                          # Creamos una variable para establecer el color de fondo en la pantalla
 
     while True:                                                                             # Bucle de activacion de juego
-        gameFunctions.checkEvents(ship)                                                     # Detectar eventos del teclado o raton. Metodo traido desde GameFunctions
-        ship.update()
-        gameFunctions.refreshScreen(settings, screen, ship)                                 # Actualizamos las imagenes en pantalla durante la ejecucion
+        gameFunctions.checkEvents(settings, screen, ship, bullets)                          # Detectar eventos del teclado o raton. Metodo traido desde GameFunctions
+        ship.update()                                                                       # Actualizamos los valores de la nave durante la ejecucion
+        bullets.update()                                                                    # Actualizamos los valores para cada bala durante la ejecucion
+        gameFunctions.refreshScreen(settings, screen, ship, bullets)                        # Actualizamos las imagenes en pantalla durante la ejecucion
 
 run_game()                                                                                  # Ejecutamos el metodo que inicializa el juego
